d2c1fdf7c5286df3a22603423050af6a7549ecb3
Batmansanty49 <sruizramirez.is@gmail.com>
2023-09-01 03:04:37 -0600

    Se agrega el Proyecto

---
+++ src/main/java/com/mycompany/holamundo/java/HolaMundoJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holamundo.java;
+
+/**
+ *
+ * @author Santiago Ruiz Ramírez
+ */
+public class HolaMundoJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo del grupo 08 de POO!");
+    }
+}
